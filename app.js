c975418f2d6d0acc98ed391d90d3f44bda7c4a63
Arjen Voorend <a.o.voorend@gmail.com>
2022-06-14 16:58:14 +0200

    Add grid size buttons functionality

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+const colorPicker = document.querySelector('#color-picker');
+const colorPickerChoice = document.querySelector('#color-picker-choice');
+const rainbowMode = document.querySelector('#rainbow-mode');
+const eraserMode = document.querySelector('#eraser');
+const clearAll = document.querySelector('#clear-all');
+
+const gridSizeOne = document.querySelector('#grid-size-one').addEventListener('click', () => {
+  createSketchboard(16);
+});
+const gridSizeTwo = document.querySelector('#grid-size-two').addEventListener('click', () => {
+  createSketchboard(32);
+});
+const gridSizeThree = document.querySelector('#grid-size-three').addEventListener('click', () => {
+  createSketchboard(48)
+});
+const gridSizeFour = document.querySelector('#grid-size-four').addEventListener('click', () => {
+  createSketchboard(64);
+});
+
+
+
 // create sketchboard divs and add them to the sketchboard container
 const sketchboardContainer = document.querySelector('.container');
 
-function createSketchboard(gridSize) {
+function createSketchboard(gridSize = 16) {
   for (let i = 0; i < (gridSize * gridSize); i++) {
     let div = document.createElement('div');
     div.classList.add('sketchboard');
@@ -10,7 +31,7 @@ function createSketchboard(gridSize) {
   }
 }
 
-createSketchboard(16);
+createSketchboard();
 
 
 // add random background colors to the sketchboard divs on hover
